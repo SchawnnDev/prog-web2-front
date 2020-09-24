94b80533c92a996f4dc87cc0e5a2d0aba83cdae7
SchawnnDev <iperskill@gmail.com>
2020-09-24 19:20:27 +0200

    designed header & started home page

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,12 +1,14 @@
 <template>
-  <nav class="navbar">
-    <img src="https://trypyramid.com/img/pyramid-logo-white-horizontal.png" alt="test logo" class="nav-logo"/>
-    <ul class="nav">
-      <li class="nav-item"><a class="nav-link" href="#">Accueil</a></li>
-      <li class="nav-item"><a class="nav-link" href="#">Photos</a></li>
-      <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
-    </ul>
-  </nav>
+  <header class="header">
+    <nav class="navbar">
+      <img src="../../assets/logo.png" alt="test logo" class="nav-logo"/>
+      <ul class="nav">
+        <li class="nav-item active"><a class="nav-link" href="home">Accueil</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Photos</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+      </ul>
+    </nav>
+  </header>
 </template>
 
 <script>
@@ -19,18 +21,21 @@ export default {
 
 @import url(https://fonts.googleapis.com/css?family=Karla);
 
-.navbar {
+.header {
   width: 100%;
   position: sticky;
   position: -webkit-sticky;
   top: 0;
+}
+
+.navbar {
   background-color: var(--bg-primary);
   font-size: 18px;
   overflow: hidden;
   display: grid;
   grid-template-columns: 1fr 3fr;
+  grid-template-rows: minmax(0, 60px);
   align-items: center;
-  height: 50px;
 }
 
 .nav {
@@ -46,20 +51,31 @@ export default {
   height: 100%;
 }
 
+.nav-item a {
+  transition: color 0.2s ease-out;
+  -webkit-transition: color 0.2s ease-out;
+}
+
+.nav-item.active a {
+  color: orange;
+}
+
+.nav-item a:hover {
+  color: orangered;
+}
+
 .nav-link {
-  color: whitesmoke;
+  color: white;
   text-decoration: none;
 }
 
-.nav-item .active, a:hover {
-  color: olive;
-}
 
 .nav-logo {
-  height: 40px;
   width: auto;
   justify-self: start;
   margin-left: 20px;
+  margin-right: 30px;
+  padding: 10px;
 }
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+
+    <section class="article-header">
+      <h1>Bienvenue sur le site de l'association</h1>
+      <p> des étudiants de médecine de la faculté de Strasbourg</p>
+    </section>
+
   </div>
 </template>
 
@@ -8,7 +14,32 @@
 
 export default {
   name: 'Home',
-  components: {
-  }
+  components: {}
 }
 </script>
+
+<style scoped>
+.article-header {
+  width: 100%;
+  min-height: 300px;
+  background: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.7));
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-size: cover;
+}
+
+.article-header h1, p {
+  color: white;
+  text-align: center;
+}
+
+.article-header h1 {
+  font-size: 36px;
+}
+
+.article-header p {
+  font-size: 20px;
+}
+
+</style>
